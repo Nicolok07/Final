610c4512e5ce305a762c96c10aa697e853eb2668
estudiante#1 <nicolas.muneraj.col@upb.edu.co>
2025-05-17 00:08:59 -0500

    se agrego la opcion de volver a jugar cuando el juego se acaba

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -2,22 +2,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoAdivinanza {
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-
+        String jugarOtraVez;
+        
         System.out.println("Bienvenido al juego de adivinanzas");
         System.out.println("En este juego tendrás que adivinar un número entre el 1 y el 50, podrás decidir si quieres tener 5, 10 o 15 intentos. Buena suerte.");
+        System.out.println("\n¿Cuántos intentos quieres tener? (5, 10 o 15): ");
 
-        int numeroSecreto = random.nextInt(50) + 1; 
-        int intentos = 0;
-        int maxIntentos = 0;
-        int intentoUsuario = -1; 
+        do{
+           int numeroSecreto = random.nextInt(50) + 1; 
+            int intentos = 0;
+            int maxIntentos = 0;
+            int intentoUsuario = -1; 
 
-
-       
         while (true) {
             maxIntentos = scanner.nextInt();
             if (maxIntentos == 5 || maxIntentos == 10 || maxIntentos == 15) {
@@ -48,6 +48,11 @@ public class JuegoAdivinanza {
             System.out.println("El número secreto era: " + numeroSecreto);
         }
 
+          System.out.print("\n¿Quieres jugar otra vez? (s/n): ");
+            jugarOtraVez = scanner.next();
+
+        } while (jugarOtraVez.equalsIgnoreCase("s"));
+        
         System.out.println("Gracias por jugar.");
         scanner.close();
     }
